14dc92bd4ce3276683fef92c29104e969053e5d0
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-03-12 21:38:31 +0100

    Clean Code

--- src/Main.java
+++ src/Main.java
@@ -12,36 +12,24 @@ public class Main {
 
   // красиво вывести состав групп на экран
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     File studentsFile = new File("res/studentslist.txt");
-    BufferedReader fileReader = new BufferedReader(new FileReader(studentsFile));
     List<Student> students = new ArrayList<>();
     // прочитать количество групп
-    int groups = 1;
-    for (int groupId = 0; groupId < groups; ++groupId) {
-      // для каждой группы:
-      readGroup(br, students, studentsFile);
-    }
+    readGroup(students, studentsFile);
     for (Student student : students) {
       System.out.printf("В группе (%s) %s :%s%n", student.getGroup(), student.getName(),
           student.getEMail());
     }
   }
-
   // - прочитать название группы
   // - прочитать количество студентов
   // - прочитать информацию о студентах - "имя" или "имя,e-mail" для каждого в отдельной строке
-  private static void readGroup(BufferedReader br, List<Student> students, File studentsFile) throws IOException {
+  private static void readGroup(List<Student> students, File studentsFile) throws IOException {
     BufferedReader fileReader = new BufferedReader(new FileReader(studentsFile));
-
-    //int studentsNumber = Integer.parseInt(br.readLine());
     for (String line = fileReader.readLine(); line != null; line = fileReader.readLine()) {
       String groupName = line.substring(0, line.indexOf(":"));
-      //String line = br.readLine();
       Student student = Student.parseStudent(groupName, line);
       students.add(student);
     }
-
   }
 }
